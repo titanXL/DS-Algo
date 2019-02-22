2fb904cd8b38a0af90261b5de47844434797e20a
Davidkov <emil.davidkov88@gmail.com>
2019-02-22 16:57:30 +0200

    With prev priorityQueue

--- weightedGraph.js
+++ weightedGraph.js
@@ -1,17 +1,79 @@
-class PriorityQueue {
-    constructor(){
-      this.values = [];
+class Node {
+    constructor(val, priority) {
+        this.val = val
+        this.priority = priority
     }
-    enqueue(val, priority) {
-      this.values.push({val, priority});
-      this.sort();
-    };
+}
+
+class PriorityQueue {
+    constructor() {
+      this.values = []
+    }
+    enqueue(value, priority) {
+        const node = new Node(value, priority)
+      this.values.push(node)
+      this._bubbleUp()
+    }
+  
     dequeue() {
-      return this.values.shift();
-    };
-    sort() {
-      this.values.sort((a, b) => a.priority - b.priority);
-    };
+      const min = this.values[0]
+      const end = this.values.pop()
+      if(this.values.length > 0 ) {
+        this.values[0] = end
+        this._sinkDown()
+      }
+  
+      return min
+    }
+  
+    _sinkDown() {
+      let index = 0
+      const length = this.values.length
+      const element = this.values[0]
+  
+      while(true) {
+        let leftChildIndex = 2 * index + 1
+        let rightChildIndex = 2*index + 2
+  
+        let leftChild, rightChild
+        let swap = null
+  
+        if(leftChildIndex< length) {
+          leftChild = this.values[leftChildIndex]
+          if(leftChild.priority < element.priority) {
+            swap = leftChildIndex
+          }
+        }
+        if(rightChildIndex < length) {
+          rightChild = this.values[rightChildIndex]
+          if((swap === null && rightChild.priority < element.priority) || (swap !== null && rightChild.priority < leftChild.priority)) {
+            swap = rightChildIndex
+          }
+        }
+        if(swap == null) break
+        this.values[index] = this.values[swap]
+        this.values[swap] = element
+        index = swap
+      }
+    }
+  
+    _bubbleUp() {
+      let index = this.values.length - 1
+      let element = this.values[index]
+      while(index > 0) {
+        let parentIndex = Math.floor((index - 1) / 2)
+        let parent = this.values[parentIndex]
+        if(element.priority > parent.priority) break
+        this.values[parentIndex] = element
+        this.values[index] = parent
+        index = parentIndex
+      }
+    }
+    _swap(arr, index1 , index2) {
+      var temp = arr[index1]
+      arr[index1] = arr[index2]
+      arr[index2] = temp
+    }
   }
   
   class WeightedGraph {
